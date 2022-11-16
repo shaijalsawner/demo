e0395aed875e656962d7b5d533a5739c3041cf9c
shaijal <sawnershaijal@gmail.com>
2022-11-16 14:02:52 +0530

    code

---
+++ images/js/demo.js
@@ -0,0 +1,11 @@
+//   let book = [
+//             ["math","300"],
+//             ["science","200"],
+//             ["bio","300"]
+//         ];
+//         let fetch = book[0][1];
+//         console.log(fetch);
+
+// let x = new Date();
+
+// document.write(x);
---
+++ images/js/script.js
@@ -0,0 +1,7 @@
+let age = 14;
+
+if(age <= 14){
+alert("you can vote");
+} else{
+    alert("you cant vote!");
+}
